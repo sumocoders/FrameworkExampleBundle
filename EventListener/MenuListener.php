4db2577428da44b775ab4ee27b842c4c8a15013a
Tijs Verkoyen <tijs@verkoyen.eu>
2015-03-11 12:08:35 +0100

    Show the hello action in the menu

---
+++ EventListener/MenuListener.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace SumoCoders\FrameworkExampleBundle\EventListener;
+
+use SumoCoders\FrameworkCoreBundle\Event\ConfigureMenuEvent;
+use SumoCoders\FrameworkCoreBundle\EventListener\DefaultMenuListener;
+
+class MenuListener extends DefaultMenuListener
+{
+    public function onConfigureMenu(ConfigureMenuEvent $event)
+    {
+        /** @var \SumoCoders\FrameworkUserBundle\Entity\User $user */
+        $user = $this->getSecurityContext()->getToken()->getUser();
+        $menu = $event->getMenu();
+
+        if ($user) {
+            $menuItem = $event->getFactory()->createItem(
+                'menu.example.overview',
+                array(
+                    'uri' => '#',
+                    'label' => 'menu.example.overview',
+                )
+            );
+            $menuItem->setChildrenAttribute('class', 'subNavigation');
+            $menuItem->setLinkAttribute('class', 'toggleSubNavigation');
+
+            $menuItem->addChild(
+                'menu.example.hello',
+                array(
+                    'route' => 'sumocoders_frameworkexample_default_hello',
+                    'routeParameters' => array(
+                        'name' => $user->getUsername(),
+                    ),
+                )
+            );
+
+            $menu->addChild($menuItem);
+        }
+    }
+}
