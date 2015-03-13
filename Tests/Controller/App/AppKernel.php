7a5c836507b11718ae4508ec6a7713187e710c4f
Tijs Verkoyen <tijs@verkoyen.eu>
2015-03-13 16:41:44 +0100

    Cleanup

--- Tests/Controller/App/AppKernel.php
+++ Tests/Controller/App/AppKernel.php
@@ -14,8 +14,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-//            new JMS\SerializerBundle\JMSSerializerBundle($this),
-//            new FOS\RestBundle\FOSRestBundle(),
 
             new SumoCoders\FrameworkExampleBundle\SumoCodersFrameworkExampleBundle(),
         );
