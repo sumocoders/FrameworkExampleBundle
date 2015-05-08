225ebee1daf4879d0d7eea82e0257ea9ecf5a999
Tijs Verkoyen <tijs@verkoyen.eu>
2015-05-08 16:02:19 +0200

    Added the form into a Type

--- Controller/TutorialController.php
+++ Controller/TutorialController.php
@@ -2,11 +2,11 @@
 
 namespace SumoCoders\FrameworkExampleBundle\Controller;
 
+use SumoCoders\FrameworkExampleBundle\Form\Type\DatePickerType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Validator\Constraints\DateTime;
 
 class TutorialController extends Controller
 {
@@ -39,167 +39,7 @@ class TutorialController extends Controller
             'birthday_example4' => new \DateTime(),
         );
 
-        $form = $this->createFormBuilder($defaultData)
-            ->add(
-                'date_example1',
-                'date',
-                array(
-                    'widget' => 'choice',
-                )
-            )
-            ->add(
-                'date_example2',
-                'date',
-                array(
-                    'widget' => 'text',
-                    'required' => false,
-                )
-            )
-            ->add(
-                'date_example3',
-                'date',
-                array(
-                    'widget' => 'single_text',
-                    'required' => false,
-                )
-            )
-            ->add(
-                'date_example4',
-                'date',
-                array(
-                    'widget' => 'single_text',
-                    'datepicker' => true,
-                    'required' => false,
-                )
-            )
-            ->add(
-                'date_example5',
-                'date',
-                array(
-                    'widget' => 'single_text',
-                    'datepicker' => true,
-                    'date_type' => 'normal',
-                    'required' => false,
-                )
-            )
-            ->add(
-                'date_example6',
-                'date',
-                array(
-                    'widget' => 'single_text',
-                    'datepicker' => true,
-                    'date_type' => 'start',
-                    'minimum_date' => new \DateTime('last monday'),
-                    'required' => false,
-                )
-            )
-            ->add(
-                'date_example7',
-                'date',
-                array(
-                    'widget' => 'single_text',
-                    'datepicker' => true,
-                    'date_type' => 'until',
-                    'maximum_date' => new \DateTime('next friday'),
-                    'required' => false,
-                )
-            )
-            ->add(
-                'date_example8',
-                'date',
-                array(
-                    'widget' => 'single_text',
-                    'datepicker' => true,
-                    'date_type' => 'range',
-                    'minimum_date' => new \DateTime('last monday'),
-                    'maximum_date' => new \DateTime('next friday'),
-                    'required' => false,
-                )
-            )
-            ->add(
-                'datetime_example1',
-                'datetime',
-                array(
-                    'widget' => 'choice',
-                    'required' => false,
-                )
-            )
-            ->add(
-                'datetime_example2',
-                'datetime',
-                array(
-                    'widget' => 'text',
-                    'required' => false,
-                )
-            )
-            ->add(
-                'datetime_example3',
-                'datetime',
-                array(
-                    'widget' => 'single_text',
-                    'required' => false,
-                )
-            )
-            ->add(
-                'time_example1',
-                'time',
-                array(
-                    'widget' => 'choice',
-                    'required' => false,
-                )
-            )
-            ->add(
-                'time_example2',
-                'time',
-                array(
-                    'widget' => 'text',
-                    'required' => false,
-                )
-            )
-            ->add(
-                'time_example3',
-                'time',
-                array(
-                    'widget' => 'single_text',
-                    'required' => false,
-                )
-            )
-            ->add(
-                'birthday_example1',
-                'birthday',
-                array(
-                    'widget' => 'choice',
-                    'required' => false,
-                )
-            )
-            ->add(
-                'birthday_example2',
-                'birthday',
-                array(
-                    'widget' => 'text',
-                    'required' => false,
-                )
-            )
-            ->add(
-                'birthday_example3',
-                'birthday',
-                array(
-                    'widget' => 'single_text',
-                    'required' => false,
-                )
-            )
-            ->add(
-                'birthday_example4',
-                'birthday',
-                array(
-                    'widget' => 'single_text',
-                    'datepicker' => true,
-                    'required' => false,
-                )
-            )
-            ->getForm();
-
-        $form->handleRequest($request);
+        $form = $this->createForm(new DatePickerType(), $defaultData);
 
         return array(
             'form' => $form->createView(),
---
+++ Form/Type/DatePickerType.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace SumoCoders\FrameworkExampleBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class DatePickerType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'date_example1',
+                'date',
+                array(
+                    'widget' => 'choice',
+                )
+            )
+            ->add(
+                'date_example2',
+                'date',
+                array(
+                    'widget' => 'text',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'date_example3',
+                'date',
+                array(
+                    'widget' => 'single_text',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'date_example4',
+                'date',
+                array(
+                    'widget' => 'single_text',
+                    'datepicker' => true,
+                    'required' => false,
+                )
+            )
+            ->add(
+                'date_example5',
+                'date',
+                array(
+                    'widget' => 'single_text',
+                    'datepicker' => true,
+                    'date_type' => 'normal',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'date_example6',
+                'date',
+                array(
+                    'widget' => 'single_text',
+                    'datepicker' => true,
+                    'date_type' => 'start',
+                    'minimum_date' => new \DateTime('last monday'),
+                    'required' => false,
+                )
+            )
+            ->add(
+                'date_example7',
+                'date',
+                array(
+                    'widget' => 'single_text',
+                    'datepicker' => true,
+                    'date_type' => 'until',
+                    'maximum_date' => new \DateTime('next friday'),
+                    'required' => false,
+                )
+            )
+            ->add(
+                'date_example8',
+                'date',
+                array(
+                    'widget' => 'single_text',
+                    'datepicker' => true,
+                    'date_type' => 'range',
+                    'minimum_date' => new \DateTime('last monday'),
+                    'maximum_date' => new \DateTime('next friday'),
+                    'required' => false,
+                )
+            )
+            ->add(
+                'datetime_example1',
+                'datetime',
+                array(
+                    'widget' => 'choice',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'datetime_example2',
+                'datetime',
+                array(
+                    'widget' => 'text',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'datetime_example3',
+                'datetime',
+                array(
+                    'widget' => 'single_text',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'time_example1',
+                'time',
+                array(
+                    'widget' => 'choice',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'time_example2',
+                'time',
+                array(
+                    'widget' => 'text',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'time_example3',
+                'time',
+                array(
+                    'widget' => 'single_text',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'birthday_example1',
+                'birthday',
+                array(
+                    'widget' => 'choice',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'birthday_example2',
+                'birthday',
+                array(
+                    'widget' => 'text',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'birthday_example3',
+                'birthday',
+                array(
+                    'widget' => 'single_text',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'birthday_example4',
+                'birthday',
+                array(
+                    'widget' => 'single_text',
+                    'datepicker' => true,
+                    'required' => false,
+                )
+            )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'datePicker';
+    }
+}
